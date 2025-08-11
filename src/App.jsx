74f35e6838a9cf3b73ba9af361fe009eed1e87b4
will323071 <cylee323071@gmail.com>
2025-08-11 06:33:45 +0000

    [React] 4일차 과제 제출

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import Cart from './components/Cart';
 
 const App = () => {
     const [cart, setCart] = useState([]);
-
+    const newCart = cart.slice();
     const addToCart = (product) => {
         // TODO: 카트에 상품을 추가하는 함수를 완성하세요.
         // 상품은 배열객체로 이루어져 있습니다.
         // 카트에 상품이 8개 이상이면 alert를 띄워
         // 더 이상 상품을 추가할 수 없다고 알려주세요.
+        newCart.push(product);
+        console.log(newCart);
+        if(newCart.length > 8) {
+            alert('더 이상 상풍을 추가할 수 없습니다.')
+        }else{
+            setCart(newCart);
+        }
     };
 
     return (
